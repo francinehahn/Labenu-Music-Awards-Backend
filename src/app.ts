8047e6d831a42d1c27c2edb04955fa84d2ce95be
Francine Hahn <fran_hahn@hotmail.com>
2024-01-14 18:12:07 -0300

    refactor: cors

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,12 @@ dotenv.config()
 export const app = express()
 app.use(express.json())
 app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://labenu-music-awards-frontend.vercel.app/')
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+})
 
 app.listen(process.env.PORT || 3003, () => {
     console.log(`Server is running on port ${process.env.PORT || 3003}`)
